5c715627af75fb9c9f66c673487ccd6a65e6a36a
Wheezy <32607367+we-z@users.noreply.github.com>
2022-12-30 11:14:58 -0800

    Update Algo.py

--- Algo.py
+++ Algo.py
@@ -10,67 +10,70 @@ SECRET_KEY = "YOUR_SECRET_KEY"
 api = tradeapi.REST(API_KEY, SECRET_KEY, api_version='v2')
 
 # Set the time frame for the analysis
-timeframe = "1D"
+timeframe = "1Min"
 
 # Set the list of ticker symbols to analyze
 ticker_list = ["SPY", "SQQQ", "TQQQ"]
 
 # Loop through the ticker symbols
-for ticker in ticker_list:
-    # Get the historical data for the ticker
-    barset = api.get_barset(ticker, timeframe, limit=1000).df
+while True:
+    for ticker in ticker_list:
+        # Get the historical data for the ticker
+        barset = api.get_barset(ticker, timeframe, limit=1000).df
 
-    # Calculate the MACD indicator
-    fast_period = 12
-    slow_period = 26
-    signal_period = 9
+        # Calculate the MACD indicator
+        fast_period = 3
+        slow_period = 15
+        signal_period = 2
 
-    barset['fast_ema'] = barset[ticker].ewm(span=fast_period, adjust=False).mean()
-    barset['slow_ema'] = barset[ticker].ewm(span=slow_period, adjust=False).mean()
-    barset['macd'] = barset['fast_ema'] - barset['slow_ema']
-    barset['signal'] = barset['macd'].ewm(span=signal_period, adjust=False).mean()
+        barset['fast_ema'] = barset[ticker].ewm(span=fast_period, adjust=False).mean()
+        barset['slow_ema'] = barset[ticker].ewm(span=slow_period, adjust=False).mean()
+        barset['macd'] = barset['fast_ema'] - barset['slow_ema']
+        barset['signal'] = barset['macd'].ewm(span=signal_period, adjust=False).mean()
+        
+        print(barset)
 
-    # Get the current time in the Pacific time zone
-    pacific_time = pytz.timezone('US/Pacific')
-    current_time = pd.datetime.now(pacific_time)
+        # Get the current time in the Pacific time zone
+        pacific_time = pytz.timezone('US/Pacific')
+        current_time = pd.datetime.now(pacific_time)
 
-    # Check if the MACD indicator is above the signal line
-    if barset['macd'][-1] > barset['signal'][-1]:
-        # MACD is above the signal line, so place a buy order
-        try:
-            api.submit_order(
-                symbol=ticker,
-                qty=1,
-                side='buy',
-                type='market',
-                time_in_force='gtc',
-                order_class='bracket',
-                take_profit=dict(
-                    limit_price=barset[ticker][-1]*1.03
-                ),
-                stop_loss=dict(
-                    stop_price=barset[ticker][-1]*0.97,
-                    limit_price=barset[ticker][-1]*0.97
+        # Check if the MACD indicator is above the signal line
+        if barset['macd'][-1] > barset['signal'][-1]:
+            # MACD is above the signal line, so place a buy order
+            try:
+                api.submit_order(
+                    symbol=ticker,
+                    qty=1,
+                    side='buy',
+                    type='market',
+                    time_in_force='gtc',
+                    order_class='bracket',
+                    take_profit=dict(
+                        limit_price=barset[ticker][-1]*1.03
+                    ),
+                    stop_loss=dict(
+                        stop_price=barset[ticker][-1]*0.97,
+                        limit_price=barset[ticker][-1]*0.97
+                    )
                 )
-            )
-        except Exception as e:
-            print("Error placing buy order:", e)
-    else:
-        # MACD is below the signal line, so place a sell order
-        try:
-            api.submit_order(
-                symbol=ticker,
-                qty=1,
-                side='sell',
-                type='market',
-                time_in_force='gtc',
-                order_class='bracket',
-                take_profit=dict(
-                    limit_price=barset[ticker][-1]*0.97
-                ),
-                stop_loss=dict(
-                    stop_price=barset[ticker][-1]*1.03,
-                    limit_price=barset[ticker][-1]*1.03
-                )
-         except Exception as e:
-             print("Error placing sell order:", e)
+            except Exception as e:
+                print("Error placing buy order:", e)
+        else:
+            # MACD is below the signal line, so place a sell order
+            try:
+                api.submit_order(
+                    symbol=ticker,
+                    qty=1,
+                    side='sell',
+                    type='market',
+                    time_in_force='gtc',
+                    order_class='bracket',
+                    take_profit=dict(
+                        limit_price=barset[ticker][-1]*0.97
+                    ),
+                    stop_loss=dict(
+                        stop_price=barset[ticker][-1]*1.03,
+                        limit_price=barset[ticker][-1]*1.03
+                    )
+             except Exception as e:
+                 print("Error placing sell order:", e)
